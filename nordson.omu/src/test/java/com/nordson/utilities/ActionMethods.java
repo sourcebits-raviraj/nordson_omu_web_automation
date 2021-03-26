b1bebb34d353fd44230f2b5bf94f4a40b5aeec75
Jayasena <Jayasena.mallikarjun@ascendum.com>
2021-03-26 12:45:14 +0530

    MDS Pressure Files

--- nordson.omu/src/test/java/com/nordson/utilities/ActionMethods.java
+++ nordson.omu/src/test/java/com/nordson/utilities/ActionMethods.java
@@ -5,9 +5,14 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.nordson.testCases.BaseClass;
 
@@ -30,5 +35,38 @@ public class ActionMethods extends BaseClass {
 		String generatedString2 = RandomStringUtils.randomNumeric(4);
 		return (generatedString2);
 	}
+
+	public void waitForAnElementPresence(By string) {
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated((string)));
+		}
+	
+	public void waitForAnElementClickable(By string) {
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions.elementToBeClickable((string)));
+		}
+	public void waitFortexttoBePresent(final By byObject)
+	{
+		
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+
+		wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return (d.findElement(byObject).getAttribute ("value").length() >= 1);
+            }
+        }); 
+	}
+
+	public void waitForAnElementPresence(WebElement element) {
+		// TODO Auto-generated method stub
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		wait.until(ExpectedConditions.visibilityOf((element)));
+	}
+	public void waitForAnElementIsInVisible(By element) throws Error 
+	{
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(element));
+    }
+	
 
 }
--- nordson.omu/src/test/java/com/nordson/utilities/ReadConfig.java
+++ nordson.omu/src/test/java/com/nordson/utilities/ReadConfig.java
@@ -9,59 +9,108 @@ public class ReadConfig {
 	Properties pro;
 
 	public ReadConfig() {
-
 		File src = new File("./configs/config.properties");
-
 		try {
 			FileInputStream fis = new FileInputStream(src);
 			pro = new Properties();
 			pro.load(fis);
-		}
-
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Expecption is " + e.getMessage());
 		}
 	}
 
 	public String getAppURL() {
-
 		String url = pro.getProperty("baseURL");
 		return url;
-
 	}
 
 	public String getSuperAdminUsername() {
-
 		String uname = pro.getProperty("superadminusername");
 		return uname;
-
 	}
 
 	public String getSuperAdminPassword() {
-
 		String pwd = pro.getProperty("superadminpassword");
 		return pwd;
-
 	}
 
 	public String getChromepath() {
-
 		String chropath = pro.getProperty("chromepath");
 		return chropath;
-
 	}
 
 	public String getFirepath() {
-
 		String chropath = pro.getProperty("firepath");
 		return chropath;
-
 	}
 
 	public String getIEpath() {
 
 		String chropath = pro.getProperty("IEpath");
 		return chropath;
+
+	}
+
+	public String getExcelMDSpathPressure() {
+		String MDSpath = pro.getProperty("MDSpathPressure");
+		return MDSpath;
+
+	}
+// Pressure Properties to be fetched from config file
+	public String getUIfieldTobefetched_MinimumPressureAlrt() {
+		return pro.getProperty("UIfildTobefetched_MinPA");
+	}
+
+	public String getUIfieldTobefetched_MaximumPressureAlrt() {
+		return pro.getProperty("UIfildTobefetched_MaxPA");
+	}
+
+	public String getUIfieldTobefetched_PressureSetPoint() {
+		return pro.getProperty("UIfildTobefetched_PressureSetPoint");
+	}
+
+	public String getUIfieldTobefetched_LowPressureAlertThreshold() {
+		return pro.getProperty("UIfildTobefetched_LowPressureAlertThreshold");
+	}
+
+	public String getUIfieldTobefetched_HighPressureAlertThreshold() {
+		return pro.getProperty("UIfildTobefetched_HighPressureAlertThreshold");
+	}
+
+	public String getUIfieldTobefetched_MinimumPressureSetPointRange() {
+		return pro.getProperty("UIfildTobefetched_MinimumPressureSetPointRange");
+	}
+
+	public String getUIfieldTobefetched_MaximumPressureSetPointRange() {
+		return pro.getProperty("UIfildTobefetched_MaximumPressureSetPointRange");
+	}
+
+	public String getUIfieldTobefetched_LowSpeedPressureSetting() {
+		return pro.getProperty("UIfildTobefetched_LowSpeedPressureSetting");
+	}
+
+	public String getUIfieldTobefetched_LowLineSpeedSetting() {
+		return pro.getProperty("UIfildTobefetched_LowLineSpeedSetting");
+	}
+
+	public String getUIfieldTobefetched_HighSpeedPressureSetting() {
+		return pro.getProperty("UIfildTobefetched_HighSpeedPressureSetting");
+	}
+
+	public String getUIfieldTobefetched_HighLineSpeedSetting() {
+		return pro.getProperty("UIfildTobefetched_HighLineSpeedSetting");
+	}
+
+	public String getUIfieldTobefetched_SetMaximumPressureLimit() {
+		return pro.getProperty("UIfildTobefetched_SetMaximumPressureLimit");
+	}
+
+	public String getUIfieldTobefetched_SetMinimumPressureLimit() {
+		return pro.getProperty("UIfildTobefetched_SetMinimumPressureLimit");
+	}
+
+	public String getUIfieldTobefetched_FullScaleLineSpeed() {
+		return pro.getProperty("UIfildTobefetched_FullScaleLineSpeed");
 
 	}
 
