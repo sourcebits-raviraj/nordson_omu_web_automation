bf4b51e53f69ecd662e9b5da8a69f53c8004d124
sourcebits-raviraj <60780896+sourcebits-raviraj@users.noreply.github.com>
2021-03-31 12:26:22 +0530

    Revert "Retrive mds file val pressure"

--- nordson.omu/src/test/java/com/nordson/utilities/ReadConfig.java
+++ nordson.omu/src/test/java/com/nordson/utilities/ReadConfig.java
@@ -9,39 +9,53 @@ public class ReadConfig {
 	Properties pro;
 
 	public ReadConfig() {
+
 		File src = new File("./configs/config.properties");
+
 		try {
 			FileInputStream fis = new FileInputStream(src);
 			pro = new Properties();
 			pro.load(fis);
-		} catch (Exception e) {
+		}
+
+		catch (Exception e) {
 			System.out.println("Expecption is " + e.getMessage());
 		}
 	}
 
 	public String getAppURL() {
+
 		String url = pro.getProperty("baseURL");
 		return url;
+
 	}
 
 	public String getSuperAdminUsername() {
+
 		String uname = pro.getProperty("superadminusername");
 		return uname;
+
 	}
 
 	public String getSuperAdminPassword() {
+
 		String pwd = pro.getProperty("superadminpassword");
 		return pwd;
+
 	}
 
 	public String getChromepath() {
+
 		String chropath = pro.getProperty("chromepath");
 		return chropath;
+
 	}
 
 	public String getFirepath() {
+
 		String chropath = pro.getProperty("firepath");
 		return chropath;
+
 	}
 
 	public String getIEpath() {
@@ -50,68 +64,94 @@ public class ReadConfig {
 		return chropath;
 
 	}
+	
+	public String sheetname() {
 
-	public String getExcelMDSpathPressure() {
-		String MDSpath = pro.getProperty("MDSpathPressure");
+		String sheetname = pro.getProperty("sheetname");
+		return sheetname;
+
+	}
+
+
+	public String getExcelMDSpath() {
+
+		String MDSpath = pro.getProperty("MDSpath");
 		return MDSpath;
 
 	}
-// Pressure Properties to be fetched from config file
-	public String getUIfieldTobefetched_MinimumPressureAlrt() {
-		return pro.getProperty("UIfildTobefetched_MinPA");
+
+	public String getSheetname() {
+
+		String sheetname = pro.getProperty("sheetname");
+		return sheetname;
+
 	}
 
-	public String getUIfieldTobefetched_MaximumPressureAlrt() {
-		return pro.getProperty("UIfildTobefetched_MaxPA");
+	public String getUIfieldTobefetched_GP() {
+
+		String UIfildTobefetchedGP = pro.getProperty("UIfildTobefetched_GP");
+		return UIfildTobefetchedGP;
+
 	}
 
-	public String getUIfieldTobefetched_PressureSetPoint() {
-		return pro.getProperty("UIfildTobefetched_PressureSetPoint");
+	public String getUIfieldTobefetched_TP() {
+
+		String UIfildTobefetchedTP = pro.getProperty("UIfildTobefetched_TP");
+		return UIfildTobefetchedTP;
+
 	}
 
-	public String getUIfieldTobefetched_LowPressureAlertThreshold() {
-		return pro.getProperty("UIfildTobefetched_LowPressureAlertThreshold");
+	public String getUIfieldTobefetched_MFP() {
+
+		String UIfildTobefetchedMFP = pro.getProperty("UIfildTobefetched_MFP");
+		return UIfildTobefetchedMFP;
+
 	}
 
-	public String getUIfieldTobefetched_HighPressureAlertThreshold() {
-		return pro.getProperty("UIfildTobefetched_HighPressureAlertThreshold");
+	public String getUIfieldTobefetched_HoseApp() {
+
+		String UIfildTobefetchedHAP = pro.getProperty("UIfildTobefetched_Hose/Applicator");
+		return UIfildTobefetchedHAP;
+
 	}
 
-	public String getUIfieldTobefetched_MinimumPressureSetPointRange() {
-		return pro.getProperty("UIfildTobefetched_MinimumPressureSetPointRange");
+	public String getUIfieldTobefetched_OTT() {
+
+		String UIfildTobefetchedOTT = pro.getProperty("UIfildTobefetched_OTT");
+		return UIfildTobefetchedOTT;
+
 	}
 
-	public String getUIfieldTobefetched_MaximumPressureSetPointRange() {
-		return pro.getProperty("UIfildTobefetched_MaximumPressureSetPointRange");
+	public String getUIfieldTobefetched_UTT() {
+
+		String UIfildTobefetchedUTT = pro.getProperty("UIfildTobefetched_UTT");
+		return UIfildTobefetchedUTT;
+
 	}
 
-	public String getUIfieldTobefetched_LowSpeedPressureSetting() {
-		return pro.getProperty("UIfildTobefetched_LowSpeedPressureSetting");
+	public String getUIfieldTobefetched_Tmpstbck() {
+
+		String UIfildTobefetchedtmpstbck = pro.getProperty("UIfildTobefetched_Tempstbck");
+		return UIfildTobefetchedtmpstbck;
+
+	}
+	
+	public String getUIfieldTobefetched_SMTTmeDly() {
+
+		String UIfildTobefetchedsmttimedly = pro.getProperty("UIfildTobefetched_SMTTmeDly");
+		return UIfildTobefetchedsmttimedly;
 	}
 
-	public String getUIfieldTobefetched_LowLineSpeedSetting() {
-		return pro.getProperty("UIfildTobefetched_LowLineSpeedSetting");
+	public String getUIfieldTobefetched_AutoHeaterOffTme() {
+
+		String UIfildTobefetchedautohtrofftime = pro.getProperty("UIfildTobefetched_AutoHeaterOffTme");
+		return UIfildTobefetchedautohtrofftime;
 	}
 
-	public String getUIfieldTobefetched_HighSpeedPressureSetting() {
-		return pro.getProperty("UIfildTobefetched_HighSpeedPressureSetting");
-	}
+	public String getUIfieldTobefetched_SystemSetbckDly() {
 
-	public String getUIfieldTobefetched_HighLineSpeedSetting() {
-		return pro.getProperty("UIfildTobefetched_HighLineSpeedSetting");
-	}
-
-	public String getUIfieldTobefetched_SetMaximumPressureLimit() {
-		return pro.getProperty("UIfildTobefetched_SetMaximumPressureLimit");
-	}
-
-	public String getUIfieldTobefetched_SetMinimumPressureLimit() {
-		return pro.getProperty("UIfildTobefetched_SetMinimumPressureLimit");
-	}
-
-	public String getUIfieldTobefetched_FullScaleLineSpeed() {
-		return pro.getProperty("UIfildTobefetched_FullScaleLineSpeed");
-
+		String UIfildTobefetchedsystemsetbckdly = pro.getProperty("UIfildTobefetched_SystemSetbckDly");
+		return UIfildTobefetchedsystemsetbckdly;
 	}
 
 }
--- nordson.omu/src/test/java/com/nordson/utilities/XLUtils.java
+++ nordson.omu/src/test/java/com/nordson/utilities/XLUtils.java
@@ -88,6 +88,7 @@ public class XLUtils {
 	}
 
 	public static int getColumnindex(String xlfile, String xlsheet, int rownum) throws IOException {
+
 		fi = new FileInputStream(xlfile);
 		wb = new XSSFWorkbook(fi);
 		ws = wb.getSheet(xlsheet);
@@ -97,7 +98,6 @@ public class XLUtils {
 	}
 
 	public static int getColumnindex(String xlfile, String xlsheet, String Colnm) throws IOException {
-
 		fi = new FileInputStream(xlfile);
 		wb = new XSSFWorkbook(fi);
 		ws = wb.getSheet(xlsheet);
@@ -167,6 +167,7 @@ public class XLUtils {
 		return rwindx;
 
 	}
+
 	@DataProvider(name = "GlobalPointValues_Celsius")
 	public static String[][] getDataGP_Celsius() throws IOException {
 		String path = System.getProperty("user.dir") + "/src/test/java/com/nordson/testData/Temperature.xlsx";
